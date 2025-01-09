ae4ec3ca986e73d284ee0e6b2bf8b7d095ca6608
kancharlasaisruthi <saikancharla08@gmail.com>
2025-01-09 19:29:58 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import Editor from './Editor'
 function App() {
-
+const [html,setHtml]=useState('')
+const [js,setJs]=useState('')
+const [css,setCss]=useState('')
   return (
-    <>
-      <div>
-       hello
+    
+      <div >
+    <div className='pane top-pane'>
+     <Editor language='xml' displayName='HTML' value={html} onChange={setHtml} />
+     <Editor language='javascript' displayName='javascript' value={js} onChange={setJs}/>
+     <Editor language='css' displayName='css' value={css} onChange={setCss}/>
+    </div>
+    <div className='pane'>
+      <iframe 
+      title='op'
+      sandbox='allow-scripts'
+      frameBorder={0}
+      width="100"
+      height="100"
+      />
+    </div>
       </div>
-    </>
+   
   )
 }
 
