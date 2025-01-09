5a7cf002d646ae6cf37f415f6934b0e0639832c4
kancharlasaisruthi <saikancharla08@gmail.com>
2025-01-09 21:12:33 +0530

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Editor from "./Editor";
 
-import './App.css'
-import Editor from './Editor'
 function App() {
-const [html,setHtml]=useState('')
-const [js,setJs]=useState('')
-const [css,setCss]=useState('')
-const [srcDoc,setSrcDoc]=useState('')
-useEffect(()=>{
-const timeout=setTimeout(()=>{
-setSrcDoc(` 
-<html>
-<body>${html}</body>
-<style>${css}</style>
-<script>${js}</script>
-</html>
-`)
-return ()=>clearTimeout(timeout)
-},250)
-},[html,js,css])
+  const [html, setHtml] = useState("");
+  const [js, setJs] = useState("");
+  const [css, setCss] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, js, css]);
 
   return (
-    
-     <>
-    <div className='pane top-pane'>
-     <Editor language='xml' displayName='HTML' value={html} onChange={setHtml} />
-     <Editor language='javascript' displayName='JAVASCRIPT' value={js} onChange={setJs}/>
-     <Editor language='css' displayName='CSS' value={css} onChange={setCss}/>
-    </div>
-    <div className='pane'>
-      <iframe 
-      title='op'
-      sandbox='allow-scripts'
-      frameBorder={0}
-      width={100}
-      height={100}
-      srcDoc={srcDoc}
-      />
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
+        <Editor language="css" displayName="CSS" value={css} onChange={setCss} />
+        <Editor language="javascript" displayName="JavaScript" value={js} onChange={setJs} />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts allow-same-origin"
+          frameBorder="0"
+          style={{ width: "100%", height: "100%" }}
+          srcDoc={srcDoc}
+        />
+      </div>
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import {Controlled as ControlledEditor} from 'react-codemirror2'
+import { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FaExpandAlt, FaCompressAlt } from "react-icons/fa";
+
 function Editor(props) {
-    const{
-        language,
-        displayName,
-        value,
-        onChange
-    }=props
-    function handleChange(editor,data,value){
-        onChange(value)
-    }
-  
+  const { language, displayName, value, onChange } = props;
+
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
   return (
-    <>
-        <div className='editor-container'>
-        <div className=' editor-title'>
-           {displayName}
-           <button>O/C</button>
-        </div>
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          className="toggle-button"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          {open ? <FaCompressAlt /> : <FaExpandAlt />}
+        </button>
+      </div>
+      {open && (
         <ControlledEditor
-        onBeforeChange={handleChange}
-        value={value}
-        className='code-mirror-wrapper'
-        options={{
-            lineWrapping:true,
-            lint:true,
-            mode:language,
-            theme:'material',
-            lineNumbers:true,
-            
-        }}
+          onBeforeChange={handleChange}
+          value={value}
+          className="code-mirror-wrapper"
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            theme: "material",
+            lineNumbers: true,
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+          }}
         />
-        </div>
-        </>
-    
-  )
+      )}
+    </div>
+  );
 }
 
-export default Editor
+export default Editor;
